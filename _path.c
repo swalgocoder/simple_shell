898d20fdea629b71f5b5629862c9881c18e954f0
Daniel Bacon <daniel.bacon@holbertonschool.com>
2016-12-23 06:52:49 +0000

    _path working

--- _path.c
+++ _path.c
@@ -1,46 +1,21 @@
 #include "my_shell.h"
 
-/**
- * path_helper -
- *
- */
-
-/**
- * _path - handles the PATH
- * @path: pointer to a sting of the duplicated PATH
- *
- * Return: 0 if success
- */
 int _path(char *path)
 {
-	char *token;
+	char *token, **args;
 	int i;
 
 	token = my_strtok(path, ":"); /* split first token */
-	pathArgs = malloc(sizeof(*pathArgs) * _strlen(pathString));
-	if (pathArgs == NULL)
-		return (1);
 	i = 0;
-	while (token[i])
-	{
-		pathArgs[path_Counter] = token[i];
-		i++;
-		path_Counter++;
-	} /* works */
-	pathArgs[path_Counter] = '\0';
+	args = malloc(sizeof(token) * _strlen(path));
+	if (args == NULL)
+		return (1);
+	args[i] = token;
 	while (token != NULL) /* split rest of tokens */
 	{
-		path_Counter++;
+		i++;
 		token = my_strtok(NULL, ":");
-		i = 0;
-		while (token[i])
-		{
-			pathArgs[path_Counter] = token[i];
-			printf("pathArgs: %s\n", pathArgs);
-			printf("path_Counter: %u\n", path_Counter);
-			i++;
-			path_Counter++;
-		}
-		pathArgs[path_Counter] = '\0';
+		args[i] = token;
 	}
+	return (0); /* testing */
 }
